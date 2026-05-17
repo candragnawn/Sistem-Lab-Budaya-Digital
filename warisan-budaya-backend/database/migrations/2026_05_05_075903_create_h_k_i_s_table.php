6578cdc78a03df9e938fa7d5bda8e520f635345f
Divandra Kusuma <divandrakusuma@gmail.com>
2026-05-17 18:46:43 +0800

    nambah frontend ama migrasi

--- warisan-budaya-backend/database/migrations/2026_05_05_075903_create_h_k_i_s_table.php
+++ warisan-budaya-backend/database/migrations/2026_05_05_075903_create_h_k_i_s_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,11 @@ return new class extends Migration
         Schema::create('h_k_i_s', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("title");
+            $table->string("activity_category");
+            $table->string("type");
+            $table->string('quartile');
+            $table->date('publish_date');
         });
     }
 
