ba6a689d92c059058e4d410cec3ff3563fe3cb4f
Kecchan <keshagamers@gmail.com>
2026-05-19 09:38:10 +0700

    Menambahkan Navbar

--- warisan-budaya-frontend/siwada-app/app/layout.tsx
+++ warisan-budaya-frontend/siwada-app/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
-
+import { Navbar1 } from "@/components/navbar1";
 const geist = Geist({subsets:['latin'],variable:'--font-sans'})
 
 const fontMono = Geist_Mono({
@@ -17,13 +17,17 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html
-      lang="en"
+    <html lang="en"
       suppressHydrationWarning
       className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <header>
+            <Navbar1 />
+          </header>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
