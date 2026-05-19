fdec02d5ab0e0b0503a1c58f3f022e352eb0d101
rikyhidayat240 <rikyhidayat240@gmail.com>
2026-05-19 10:47:40 +0800

    add comment for tracking

--- warisan-budaya-backend/database/migrations/2026_05_05_075903_create_h_k_i_s_table.php
+++ warisan-budaya-backend/database/migrations/2026_05_05_075903_create_h_k_i_s_table.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * migration untuk tabel h_k_i_s
+ */
 return new class extends Migration {
     /**
      * Run the migrations.
--- warisan-budaya-backend/database/migrations/2026_05_15_130557_create_journal_managers_table.php
+++ warisan-budaya-backend/database/migrations/2026_05_15_130557_create_journal_managers_table.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * migration untuk tabel journal_managers
+ */
 return new class extends Migration {
     /**
      * Run the migrations.
