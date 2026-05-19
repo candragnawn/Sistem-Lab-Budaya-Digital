ae34cfdb0d979f7bcf19a09f89d7aafca4816e78
rikyhidayat240 <rikyhidayat240@gmail.com>
2026-05-19 11:04:28 +0800

    add: API Routes and 1 controller

--- warisan-budaya-backend/routes/api.php
+++ warisan-budaya-backend/routes/api.php
@@ -1,11 +1,31 @@
 <?php
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Api\LecturerController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\LecturerController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\DigitalAssetController;
+use App\Http\Controllers\Api\PublicationController;
+use App\Http\Controllers\Api\ResearchController;
+use App\Http\Controllers\Api\CommunityServiceController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('/lecturers/{id}', [LecturerController::class, 'show']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::apiResource('lecturers', LecturerController::class);
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('digital-assets', DigitalAssetController::class);
+    Route::apiResource('publications', PublicationController::class);
+    Route::apiResource('research', ResearchController::class);
+    Route::apiResource('community-services', CommunityServiceController::class);
+});
+
+Route::get('/public/lecturers', [LecturerController::class, 'index']);
+Route::get('/public/lecturers/{id}', [LecturerController::class, 'show']);
