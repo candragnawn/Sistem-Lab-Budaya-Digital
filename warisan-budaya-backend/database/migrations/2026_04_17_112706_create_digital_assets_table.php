b072565d94d62d1b6f604f43b014ed76bb7b1233
DivandraKusuma <divandrakusuma@gmail.com>
2026-05-19 10:04:58 +0800

    fix backend lagi dikit

--- warisan-budaya-backend/database/migrations/2026_04_17_112706_create_digital_assets_table.php
+++ warisan-budaya-backend/database/migrations/2026_04_17_112706_create_digital_assets_table.php
@@ -4,22 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('digital_assets', function (Blueprint $table) {
-           $table->id();
-           $table->foreignId('_id')->constrained()->onDelete('cascade');
-           $table->foreignId('category_id')->constrained()->onDelete('cascade');
-           $table->string('title');
-           $table->text('description')->nullable();
-           $table->string('file_path');
-           $table->json('metadata')->nullable(); 
-           $table->timestamps();
+            $table->id();
+            $table->foreignId('lecturers_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('file_path');
+            $table->json('metadata')->nullable();
+            $table->timestamps();
         });
     }
 
