6e724821b47e74f5db394ba0928018a748d8485a
DivandraKusuma <divandrakusuma@gmail.com>
2026-05-26 21:56:44 +0800

    tar lu kok file filenya ada yang ilang ilang ya
    ganti nama model biar sesuai laravel, ama ngisi relasi data profil di lecturer

--- warisan-budaya-backend/app/Http/Controllers/Api/LecturerController.php
+++ warisan-budaya-backend/app/Http/Controllers/Api/LecturerController.php
@@ -4,14 +4,33 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Lecturer;
 use Illuminate\Http\Request;
+use App\Http\Resources\LecturerResource;
 
 class LecturerController extends BaseCrudController
 {
     protected $model = Lecturer::class;
 
-    public function show($id){
-        $lecturers = Lecturer::all();
-        return view('data.index', compact('lecturers'));
+    public function index()
+    {
+        //pake pagination cok biar gak penuh ramnya
+       $lecturers = Lecturer::paginate(10);
+       return LecturerResource::collection($lecturers);
+
+    }
+
+    public function show($id)
+    {
+        $lecturer = Lecturer::with([
+            'education', 
+            'ranks', 
+            'studies', 
+            'positions', 
+            'workContracts', 
+            'teachings', 
+            'publications'
+        ])->findOrFail($id);
+
+        return new LecturerResource($lecturer);
     }
 
     public function create(Request $request){
---
+++ warisan-budaya-backend/app/Models/Profile/WorkContract.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models\Profile;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Lecturer;
+
+class WorkContract extends Model
+{
+    protected $table = 'lecturer_work_contracts';
+
+    protected $fillable = [
+        'lecturer_id',
+        'work_status',
+        'current_status',
+        'tmt'
+    ];
+
+    public function lecturer(): BelongsTo {
+        return $this->belongsTo(Lecturer::class);
+    }
+}
