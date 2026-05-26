34bfccdb39a446a144d3fa06d4175bde2a90c5ad
DivandraKusuma <divandrakusuma@gmail.com>
2026-05-26 13:37:58 +0800

    coba coba bikin route

--- warisan-budaya-backend/routes/web.php
+++ warisan-budaya-backend/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PelaksanaanPenelitian\HKIController;
+use App\Http\Controllers\Api\HKIController;
+use App\Http\Controllers\Api\LecturerController;
 
 
 Route::get('/hki', [HKIController::class, 'index'])->name('');
@@ -9,3 +10,8 @@ Route::get('/hki', [HKIController::class, 'index'])->name('');
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//lecturer
+Route::get('/lecturer', [LecturerController::class, 'show'])->name('lecturer.index');
+Route::post('/lecturer/input', [LecturerController::class, 'create'])->name('lecturer.create');
