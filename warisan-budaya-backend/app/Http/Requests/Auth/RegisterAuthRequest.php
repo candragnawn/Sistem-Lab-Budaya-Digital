c0c3f9c63d70fa9e5e7c4ef959e680c25ee910cc
DivandraKusuma <divandrakusuma@gmail.com>
2026-05-27 17:06:44 +0800

    buat request di folder auth buat login sama register

---
+++ warisan-budaya-backend/app/Http/Requests/Auth/RegisterAuthRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+class RegisterAuthRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8'
+        ];
+    }
+}
