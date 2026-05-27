323ee43c38b2d4c22769a20a4866cfc2664d0231
DivandraKusuma <divandrakusuma@gmail.com>
2026-05-27 18:13:00 +0800

    lagi sikit wak (penjelasan dibawah)
    kurang /master di importnya, udah ku cek di postman ini juga yang udah success, nah databasenya ulang cok pake php artisan migrate:fresh karna ada yang salah tu syntaxnya sama aku ngubah lecturers_hki jadi lecturer_hki

--- warisan-budaya-backend/app/Http/Controllers/Api/AuthController.php
+++ warisan-budaya-backend/app/Http/Controllers/Api/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Http\Resources\UserResource;
+use App\Http\Resources\Master\UserResource;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use App\Http\Requests\Auth\RegisterAuthRequest;
